a45521ef84b68aae61c9f7c21d52cc0d139bb92d
vanquiche <stevevang2010@hotmail.com>
2022-11-24 22:37:17 +0900

    add css modules plugin

--- src/stories/Navbar.stories.tsx
+++ src/stories/Navbar.stories.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { Nav } from '../components/index';
@@ -11,15 +9,26 @@ export default {
     text: {
       control: { type: 'text' },
     },
+    logoName: {
+      control: { type: 'text' },
+    },
   },
 } as ComponentMeta<typeof Nav>;
 
-const Template: ComponentStory<typeof Nav> = ({ text }) => {
-  return <Nav text={text} />;
+const Template: ComponentStory<typeof Nav> = ({ text, logoName }) => {
+  return <Nav text={text} logoName={logoName} />;
 };
+
+// export const Default = (args: any) => <Template {...args}/>
+
+// export const WissCSSModules = (args: any) => (
+//   <Template {...args} className={styles.Nav} />
+// );
 
 export const Primary: ComponentStory<typeof Nav> = Template.bind({});
 
 Primary.args = {
   text: '',
+  logoName: '',
+  // logoIcon: <Logo />,
 };
