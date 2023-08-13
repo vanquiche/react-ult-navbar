22968bef69ee1628316e94b5593fa6f55e3852f2
vanquiche <stevevang2010@hotmail.com>
2023-08-13 20:07:34 +0900

    refactor: move styles to stylesheet

--- src/components/NavNode/NavNode.tsx
+++ src/components/NavNode/NavNode.tsx
@@ -1,13 +1,28 @@
+import TopLevelMenuContext from '../../Contexts/TopLevelMenuContext';
 import Submenu from '../Submenu/Submenu';
 import { NavNodeType } from '../types';
-import React, { useState, useRef } from 'react';
+import { useState, useRef, useContext, RefObject, CSSProperties } from 'react';
+
+import './NavNode.css';
+
+interface Props {
+  node: NavNodeType;
+  level: number;
+}
 
 const NavNode = ({ node, level }: Props) => {
   const { topLevelMenus, setTopLevelMenus } = useContext(TopLevelMenuContext);
   const [showMenu, setShowMenu] = useState(false);
+
   const submenuRef = node.submenu ? useRef<HTMLUListElement>(null) : undefined;
   const menuitemRef = useRef<HTMLDivElement>(null);
-  const menuId = node.title.replace(' ', '-');
+  const menuId = node.title.replace(' ', '-').toLowerCase();
+
+  const menuIsTopLevel = level === 0;
+
+  const styles: CSSProperties = {
+    overflow: level > 1 ? 'hidden' : '',
+  };
 
   async function toggleMenu() {
     // for top-level menu
@@ -32,62 +47,61 @@ const NavNode = ({ node, level }: Props) => {
         return !state;
       });
 
-    // wait for submenu and menuitems to render in DOM
-    const s = new Promise<React.RefObject<HTMLUListElement>>((res, rej) => {
-      setTimeout(() => {
-        if (submenuRef) {
-          res(submenuRef);
-        } else rej(null);
-      }, 100);
-    });
+      // wait for submenu and menuitems to render in DOM
+      const menuItems = new Promise<RefObject<HTMLUListElement>>((res, rej) => {
+        setTimeout(() => {
+          if (submenuRef) {
+            res(submenuRef);
+          } else rej(null);
+        }, 100);
+      });
 
-    const m = new Promise<React.RefObject<HTMLDivElement>>((res, rej) => {
-      setTimeout(() => {
-        if (menuitemRef) {
-          res(menuitemRef);
-        } else rej(null);
-      }, 100);
-    });
+      const menu = new Promise<RefObject<HTMLDivElement>>((res, rej) => {
+        setTimeout(() => {
+          if (menuitemRef) {
+            res(menuitemRef);
+          } else rej(null);
+        }, 100);
+      });
 
-    Promise.all([s, m]).then(([sRef, mRef]) => {
-      const height = sRef?.current?.offsetHeight;
-      if (mRef && mRef.current) {
-        const level = mRef.current.getAttribute('data-menu-level');
-        if (level && +level > 0) {
-          if (menuOpened === false) {
-            mRef.current.style.paddingBottom = height + 'px';
-          } else {
-            mRef.current.style.paddingBottom = '0';
+      Promise.all([menuItems, menu]).then(([listRef, menuRef]) => {
+        const height = listRef?.current?.offsetHeight || 0;
+        if (menuRef && menuRef.current) {
+          const level = menuRef.current.getAttribute('data-menu-level');
+          if (level && +level > 0) {
+            if (currentMenuState === false) {
+              // expand menu to make room for submenu
+              menuRef.current.style.paddingBottom = height + 'px';
+              // wait for menu to expand before submenu fades in
+              setTimeout(() => {
+                if (listRef && listRef.current)
+                  listRef.current.style.opacity = '1';
+              }, 200);
+            } else {
+              // collapse menu and submenu
+              if (listRef && listRef.current)
+                listRef.current.style.opacity = '0';
+              menuRef.current.style.paddingBottom = '0';
+            }
           }
         }
-      }
-    });
+      });
+    }
   }
 
   return (
     <div
-      // onMouseLeave={handleMouseLeave}
-      // onMouseEnter={handleMouseEnter}
-      role='menuitem'
-      aria-haspopup={node.submenu ? true : false}
-      style={{
-        // outline: '1px solid red',
-        transition: 'padding-bottom 150ms ease',
-        overflow: level > 1 ? 'hidden' : '',
-        zIndex: 10,
-        position: 'relative',
-      }}
+      className='menu-item'
+      style={styles}
       ref={menuitemRef}
+      role='menuitem'
       data-menu-level={level}
+      aria-haspopup={node.submenu ? true : false}
     >
       <p
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          padding: '8px 0',
-          position: 'relative',
-          zIndex: 100,
-        }}
+        className={`menu-item__link ${
+          menuIsTopLevel ? 'top-level' : 'is-child'
+        }`}
       >
         {!node.void ? (
           <node.link {...node.linkAttribute}>{node.title}</node.link>
@@ -96,11 +110,11 @@ const NavNode = ({ node, level }: Props) => {
         )}
         {node.submenu && (
           <button
+            className='menu-item__link-caret'
             onClick={toggleMenu}
             aria-label='dropdown caret'
             aria-controls={menuId}
             aria-expanded={showMenu ? true : false}
-            style={{ marginLeft: '20px' }}
           >
             &larr;
           </button>
--- src/components/Submenu/Submenu.tsx
+++ src/components/Submenu/Submenu.tsx
@@ -5,25 +5,23 @@ import { forwardRef } from 'react';
 
 import './Submenu.css';
 
+interface Props {
+  nodes: NavNodeType[];
+  isVisible: boolean;
+  level: number;
+  id: string;
+}
+
 const Submenu = forwardRef(
   (
-    {
-      nodes,
-      isVisible,
-      level,
-      id,
-    }: {
-      nodes: NavNodeType[];
-      isVisible: boolean;
-      level: number;
-      id: string;
-    },
+    { nodes, isVisible, level, id }: Props,
     ref: ForwardedRef<HTMLUListElement>
   ) => {
     const styles: CSSProperties = {
       height: isVisible ? 'auto' : '0',
       border: level === 1 ? '1px solid black' : '',
       backgroundColor: 'aquamarine',
+      opacity: level > 1 ? 0 : 1,
     };
 
     if (nodes.length === 0) return null;
@@ -33,7 +31,9 @@ const Submenu = forwardRef(
         id={id}
         ref={ref}
         style={styles}
-        className={`submenu ${level > 1 ? 'long-fadeIn' : 'short-fadeIn'}`}
+        className={`submenu ${
+          level === 1 ? 'short-fadeIn' : 'submenu--fade child-menu'
+        }`}
         aria-hidden={isVisible ? false : true}
         role='menubar'
       >
